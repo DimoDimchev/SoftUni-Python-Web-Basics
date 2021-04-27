28dae628adc416c6749d010090592dcb9d7ce28a
Dimo Dimchev <ddimchev7@gmail.com>
2021-04-27 21:41:50 +0300

    Used more functionalities of models. Learned about Managers. Finished lectures 1 through 8

--- django_102/models/game.py
+++ django_102/models/game.py
@@ -3,7 +3,15 @@ from django.db import models
 from django_102.models.player import Player
 
 
+class GameManager(models.Manager):
+    def all_with_players_count(self):
+        return self.all()\
+                .annotate(players_count=models.Count('players'))
+
+
 class Game(models.Model):
+    objects = GameManager()
+
     DIFFICULTY_LEVEL_CHOICES = (
         ('EASY', 'Easy'),
         ('MEDIUM', 'Medium'),
@@ -12,3 +20,6 @@ class Game(models.Model):
     name = models.CharField(max_length=30, blank=False)
     difficulty_level = models.CharField(max_length=6, blank=False, choices=DIFFICULTY_LEVEL_CHOICES)
     players = models.ManyToManyField(Player)
+
+    def __str__(self):
+        return f'{self.name}'
