a145529a5c711e470f681c9f8b1f81086ebcdebf
Dimo Dimchev <ddimchev7@gmail.com>
2021-05-01 11:50:12 +0300

    Added Bootstrap to ToDo form

--- todos_app/forms.py
+++ todos_app/forms.py
@@ -2,5 +2,9 @@ from django import forms
 
 
 class TodoForm(forms.Form):
-    title = forms.CharField(max_length=30)
-    description = forms.CharField(widget=forms.Textarea, required=False)
+    title = forms.CharField(max_length=30, widget=forms.TextInput(attrs={
+        'class': 'form-control',
+    }))
+    description = forms.CharField(widget=forms.Textarea(attrs={
+        'class': 'form-control',
+    }), required=False)
