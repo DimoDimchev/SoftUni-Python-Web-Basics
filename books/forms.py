0160887d617c00be5b6c064339774cb446ef1211
Dimo Dimchev <ddimchev7@gmail.com>
2021-05-03 17:26:56 +0300

    Moved forms to forms.py

---
+++ books/forms.py
@@ -0,0 +1,31 @@
+from django import forms
+
+from books.models import Book
+
+
+class BookForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = '__all__'
+        widgets = {
+            'title': forms.TextInput(
+                attrs={
+                    'class': 'form-control'
+                }
+            ),
+            'description': forms.Textarea(
+                attrs={
+                    'class': 'form-control'
+                }
+            ),
+            'pages': forms.NumberInput(
+                attrs={
+                    'class': 'form-control'
+                }
+            ),
+            'author': forms.TextInput(
+                attrs={
+                    'class': 'form-control'
+                }
+            ),
+        }
